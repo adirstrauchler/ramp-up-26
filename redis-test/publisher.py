ad50623edcf5bf0edc76a59c257bc2deeb60d9e9
adirstrauchler <adirstrauchler@gmail.com>
2026-06-08 18:16:25 -0400

    adding redis publisher and subscriber

--- redis-test/publisher.py
+++ redis-test/publisher.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI, HTTPException
+import redis
 
+app = FastAPI()
+
+@app.post("/publish/")
+def addMessage(message:str):
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    text = r.set("message", message)
+    r.close()
+    return text
--- redis-test/subscriber.py
+++ redis-test/subscriber.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI, HTTPException
+import redis
 
+app = FastAPI()
+
+@app.get("/")
+def getMessage():
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    text = r.get("message")
+    r.close()
+    return text
