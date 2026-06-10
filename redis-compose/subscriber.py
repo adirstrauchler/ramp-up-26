4576db094b0a904043fff8bae2172edfc95c871c
adirstrauchler <adirstrauchler@gmail.com>
2026-06-10 16:16:55 -0400

    Separate redis test and redis compose folders

---
+++ redis-compose/subscriber.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, HTTPException
+import redis
+
+app = FastAPI()
+
+@app.get("/")
+def getMessage():
+    r = redis.Redis(host='redis', port=6379, decode_responses=True)
+    text = r.get("message")
+    r.close()
+    return text
